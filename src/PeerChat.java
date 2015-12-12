d5238c123bd4003f01b7a60de529090af9b5d7ae
dbaruahtcd <baruahd@tcd.ie>
2015-12-12 14:36:11 +0000

    Changed the access type

--- src/PeerChat.java
+++ src/PeerChat.java
@@ -6,11 +6,11 @@ import java.net.Socket;
 public interface PeerChat {
 	
 	
-	void init(Socket socket, int uid); //Initialize with a TCD server socket and unique id for node
-	long joinNetwork(InetSocketAddress bootstrapNode); //returns network_id, a locally  generated number to identify peer network
-	boolean leaveNetwork(long networkID); // parameter is perviously returned peer network identifier
-	void chat(String text, String[] tags);
-	ChatResult[] getChat(String[] words);
+	public void init(Socket socket, int uid); //Initialize with a TCD server socket and unique id for node
+	public long joinNetwork(InetSocketAddress bootstrapNode); //returns network_id, a locally  generated number to identify peer network
+	public boolean leaveNetwork(long networkID); // parameter is perviously returned peer network identifier
+	public void chat(String text, String[] tags);
+	public ChatResult[] getChat(String[] words);
 	
 	
 
